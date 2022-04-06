adbff6d1bd0e098f686debdef92ba7b8452c0681
Paulo Sergio <pauloserg861@gmail.com>
2022-04-05 22:39:38 -0300

    adicionando estrutura inicial de pastas

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+//defining template engine
+app.set('view engine', 'ejs')
+
+//searching views folder
+app.set('views', path.join(__dirname, 'views'))
+
+//defining public files
+app.use(express.static(path.join(__dirname, 'public')))
+
+//abble server to receive form data (post method)
+app.use(express.urlencoded({ extended: true }))
+
+//routes 
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Titulo de teste'
+    })
+})
+
+//error not found 404
+app.use((req, res) => {
+    res.send('Page not found!')
+})
+
+//running server
+const port = process.env.port || 8080
+app.listen(port, () => console.log(`server is running on port http://localhost:${port}`))
